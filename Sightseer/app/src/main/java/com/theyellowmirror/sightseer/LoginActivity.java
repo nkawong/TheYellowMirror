6e5155cccdf0b054e7bb7cfb9cc6da73102bda91
DavidL <David@dhcp-39-77-100.dhcpint.csulb.edu>
2018-03-08 16:13:28 -0800

    Menu activity created
    Deleted HomeActivity but added menu activity. Menu Activity serves as home

--- Sightseer/app/src/main/java/com/theyellowmirror/sightseer/LoginActivity.java
+++ Sightseer/app/src/main/java/com/theyellowmirror/sightseer/LoginActivity.java
@@ -31,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         skipLink.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent skipIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                Intent skipIntent = new Intent(LoginActivity.this, MenuActivity.class);
                 LoginActivity.this.startActivity(skipIntent);
             }
         });
@@ -39,26 +39,26 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_home, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
