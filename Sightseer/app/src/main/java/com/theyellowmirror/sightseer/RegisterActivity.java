2e678c0c4ad7beda4b69361cffeeca2731cc0672
Adam U Flores <watsaname1999@gmail.com>
2018-03-15 22:57:26 -0700

    Added Account Creation
    Account Creation functionality added.
    Email/Password input fields fixed.

--- Sightseer/app/src/main/java/com/theyellowmirror/sightseer/RegisterActivity.java
+++ Sightseer/app/src/main/java/com/theyellowmirror/sightseer/RegisterActivity.java
@@ -1,22 +1,106 @@
 package com.theyellowmirror.sightseer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextView register;
+    private EditText email;
+    private EditText password;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_register);
+        //Get account details from user
+        register = findViewById(R.id.registerButton);
+        email = findViewById(R.id.emailField);
+        password = findViewById(R.id.passField);
 
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
+    public void createAccount(){
+        if(!validateForm()){
+            return;
+        }
+        Log.d("Strings", password.getText().toString().trim()+email.getText().toString().trim());
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("CreateAccount", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            Intent mapScreen = new Intent(RegisterActivity.this, MenuActivity.class);
+//                            RegisterActivity.this.startActivity(mapScreen);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("CreateAccount", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String mEmail = email.getText().toString();
+        if (TextUtils.isEmpty(mEmail)) {
+            email.setError("Required.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        String mPassword = password.getText().toString();
+        if (TextUtils.isEmpty(mPassword)) {
+            password.setError("Required.");
+            valid = false;
+        } else {
+            password.setError(null);
+        }
+
+        return valid;
+    }
+
 
 }
