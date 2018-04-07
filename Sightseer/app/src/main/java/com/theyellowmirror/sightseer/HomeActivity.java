7798a9a0c6a7da39d22d74e3d7ef2b7ad94ff06b
nkawong <nkawong224@gmail.com>
2018-04-07 13:20:10 -0700

    working on just getting the marker now

--- Sightseer/app/src/main/java/com/theyellowmirror/sightseer/HomeActivity.java
+++ Sightseer/app/src/main/java/com/theyellowmirror/sightseer/HomeActivity.java
@@ -5,7 +5,9 @@ package com.theyellowmirror.sightseer;
  */
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -17,12 +19,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.Manifest;
 
 public class HomeActivity extends FragmentActivity {
 
     private static final int GPS_ERRORDIALOG_REQUEST = 9001;
     private static final int MY_PERMISSION_REQUEST = 0;
-    
+
     GoogleMap mMap;
     MapView mMapView;
 
@@ -34,9 +37,24 @@ public class HomeActivity extends FragmentActivity {
           //  mMapView = (MapView) findViewById(R.id.map);
             mMapView = (MapView) findViewById(R.id.map);
             mMapView.onCreate(savedInstanceState);
-
-
+            onMapReady(mMap);
     }
+    public void onMapReady(GoogleMap map){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.addMarker()
+        }
+    }
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        if (requestCode == MY_PERMISSION_REQUEST) {
+//            if (permissions.length == 1 &&
+//                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+//                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(permissions, MY_PERMISSION_REQUEST);
+//            }
+//        }
+//    }
 
     public boolean onCreatOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.activity_menu_drawer, menu);
