3d6d5f8f898a3fc82aa10b377a08514b6fbe6b75
nkawong <nkawong224@gmail.com>
2018-04-12 16:13:05 -0700

    this shit is broken

--- Sightseer/app/src/main/java/com/theyellowmirror/sightseer/HomeActivity.java
+++ Sightseer/app/src/main/java/com/theyellowmirror/sightseer/HomeActivity.java
@@ -15,13 +15,17 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import android.Manifest;
 
-public class HomeActivity extends FragmentActivity {
+public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int GPS_ERRORDIALOG_REQUEST = 9001;
     private static final int MY_PERMISSION_REQUEST = 0;
@@ -37,15 +41,21 @@ public class HomeActivity extends FragmentActivity {
           //  mMapView = (MapView) findViewById(R.id.map);
             mMapView = (MapView) findViewById(R.id.map);
             mMapView.onCreate(savedInstanceState);
+            //initMap();
             onMapReady(mMap);
     }
+//    public void initMap(){
+//        //MapFragment mView = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
+//        //mView.getMapAsync(this);
+//    }
     public void onMapReady(GoogleMap map){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
-            mMap.addMarker()
+            //mMap.addMarker(new MarkerOptions().position()
         }
     }
+
 //    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 //        if (requestCode == MY_PERMISSION_REQUEST) {
 //            if (permissions.length == 1 &&
@@ -60,31 +70,14 @@ public class HomeActivity extends FragmentActivity {
         getMenuInflater().inflate(R.menu.activity_menu_drawer, menu);
         return true;
     }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mMapView.onDestroy();
-    }
-    @Override
-    public void onLowMemory(){
-        mMapView.onLowMemory();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mMapView.onPause();
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        mMapView.onSaveInstanceState(outState);
-    }
-
-
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//        getGeoLocation(33.782972,-118.1191328);
+//    }
+//    public void getGeoLocation(double lat, double lng){
+//        LatLng position = new LatLng(lat, lng);
+//        CameraUpdate cam_update = CameraUpdateFactory.newLatLng(position);
+//        mMap.moveCamera(cam_update);
+//    }
 }
